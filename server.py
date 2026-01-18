e93887f51c1cc99d4e2dc6f66fe35dda0c37fb6d
Fudr007 <petr.valenta00@email.cz>
2026-01-18 13:41:17 +0100

    simple log

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import multiprocessing
 import socket
 
@@ -19,6 +20,9 @@ class Server:
         self.server_socket.bind(self.server_inet_address)
         self.server_socket.listen()
         print(f"Server started at {self.server_inet_address}")
+        with open("log.txt", "a") as f:
+            f.write(f"{str(datetime.now())} Server started at {self.server_inet_address} \n")
+
         while self._is_running:
             connection, client_inet_address = self.server_socket.accept()
             multiprocessing.Process(target=ClientHandler, args=(connection, client_inet_address, self.timeout, self.connection_info, self.log)).start()
@@ -35,6 +39,8 @@ class ClientHandler:
 
     def client(self):
         self.connection.settimeout(self.timeout)
+        with open("log.txt", "a") as f:
+            f.write(f"{str(datetime.now())} Client {self.ip} connected \n")
         try:
             with self.connection:
                 while True:
@@ -45,16 +51,24 @@ class ClientHandler:
                     data = data.replace(b"\r\n", b"")
                     response = self.commandHandling.distribute(data, self.db_connection, self.log)
                     self.connection.sendall((response + "\r\n").encode("utf-8"))
+                    with open("log.txt", "a") as f:
+                        f.write(f"{str(datetime.now())} User {self.ip}, {response} \n")
         except socket.timeout:
             self.db_connection.close()
             self.connection.close()
             print(f"Client {self.ip} timed out")
+            with open("log.txt", "a") as f:
+                f.write(f"{str(datetime.now())} Client {self.ip} timed out \n")
         except Exception as e:
             print(f"Unexpected error: {e}")
+            with open("log.txt", "a") as f:
+                f.write(f"{str(datetime.now())} Unexpected error: {e} \n")
         finally:
             self.db_connection.close()
             self.connection.close()
             print(f"Client {self.ip} disconnected")
+            with open("log.txt", "a") as f:
+                f.write(f"{str(datetime.now())} Client {self.ip} disconnected \n")
 
     def log(self, operation:str):
         with open("log.txt", "a") as f:
